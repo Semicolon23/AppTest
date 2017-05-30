2daa7b33fc3c0ae949479f5c9a98614e91972623
unknown <colinmcleodmunreo@mymail.mines.edu>
2017-05-30 17:42:06 -0600

    Tried changing things to make it work. Hopes not high

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 var express = require('express');
-var bodyParser = require('body-parser');
-var path = require('path');
 var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
 
 
 app.get('/', function(req, res){
@@ -9,8 +11,8 @@ app.get('/', function(req, res){
 });
 
 
-app.listen(5555, function(){
-	console.log('Server Started on Port 5555');
+app.listen(app.get('port'), function(){
+	console.log('Server running on port ', app.get('port'));
 })
 
 
