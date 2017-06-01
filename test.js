d6638e60a01b09b2e9eb66e05f4f1055c97d3885
unknown <colinmcleodmunreo@mymail.mines.edu>
2017-06-01 10:37:51 -0600

    Tried to add ability to read HTML file

--- test.js
+++ test.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/', function(req, res){
-	res.send('Hello World');
+fs.readFile('index.html', (err, html) => {
+	app.get('/', function(req, res){
+		res.setHeader('Content-type', 'text/html');
+		res.write(html);
+		res.end();
+	});
 });
+
+
 
 
 app.listen(app.get('port'), function(){
