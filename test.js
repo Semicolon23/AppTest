15850414af07e0936c51f57c37af2e1b6b4fcdaf
unknown <colinmcleodmunreo@mymail.mines.edu>
2017-06-06 12:23:14 -0600

    ignore

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
-var aaa = "html";
+var aaa = html;
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
