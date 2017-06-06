6d5a454a73469c278a139cdc76a9842f8b1621be
unknown <colinmcleodmunreo@mymail.mines.edu>
2017-06-06 12:24:19 -0600

    restoring

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var fs = require('fs');
-var aaa = html;
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/public'));
 fs.readFile('index.html', (err, html) => {
 	app.get('/', function(req, res){
 		res.setHeader('Content-type', 'text/html');
-		res.write(aaa);
+		res.write(html);
 		res.end();
 	});
 });
