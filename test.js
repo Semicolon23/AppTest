6e312b707d57b1a9d03c4345eb02bce9ca3edf49
unknown <colinmcleodmunreo@mymail.mines.edu>
2017-06-06 13:00:36 -0600

    -

--- test.js
+++ test.js
@@ -3,10 +3,11 @@ var app = express();
 var fs = require('fs');
 var events = require('events');
 
-var ee = new events.EventEmitter();
+var EventEmitter = require('events').EventEmitter;
+var ee = new EventEmitter();
 
 ee.on("test", function(){
-	
+	res.write('meh');
 });
 
 app.set('port', (process.env.PORT || 5000));
@@ -17,11 +18,12 @@ app.use(express.static(__dirname + '/public'));
 fs.readFile('index.html', (err, html) => {
 	app.get('/', function(req, res){
 		res.setHeader('Content-type', 'text/html');
-		console.log('Jeez');
 		res.write(html);
 		res.end();
 	});
 });
+
+ee.emit("test");
 
 app.listen(app.get('port'), function(){
 	console.log('Server running on port ', app.get('port'));
