00590420411d8a5a492084191dcb4c66a1a77f94
unknown <colinmcleodmunreo@mymail.mines.edu>
2017-06-12 12:01:00 -0600

    cleaned files

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
 //Set up server using the express framework. Listening on port 5000
-const express = require('express');
-const app = express();
-const http = require('http').Server(app).listen(5000);
-const io = require('socket.io')(http);
+var express = require('express');
+var app = express();
+var http = require('http').Server(app).listen(5000);
+var io = require('socket.io')(http);
 
+connections=[];
 //What happens when user loads main page. Can do other pages by doing
 // '/newPage' where newPage is the name of the page
 app.get('/', function(req,res){
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile(__dirname + '/testhtml.html');
 });
 
 //Client Server Communication
 io.on('connection', function(socket){
+	connections.push(socket);
+	console.log('** %s players connected', connections.length);
+	/*
+	socket.on('cardStat', function(dmg, health){
 
-	
+	});
 
+	socket.on('cardLoc', function(oldLoc, newLoc){
+
+	});
+	*/
 	socket.on('disconnect', function(){
-
+		connections.splice(connections.indexOf(socket), 1);
+		console.log('** %s players connected', connections.length);
 	});
 });
 
