ff39ea7cd751cdfd4ebe8fc5d6b5802f55645fcb
unknown <colinmcleodmunreo@mymail.mines.edu>
2017-06-12 11:15:05 -0600

    updated server code

--- test.js
+++ test.js
@@ -1,40 +1,33 @@
 const express = require('express');
 const app = express();
-//const server = require('http').createServer(app)
+const http = require('http').Server(app).listen(5000);
 const fs = require('fs');
-//const io = require('socket.io').listen(server);
+const io = require('socket.io')(http);
+
+app.get('/', function(req,res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
 
 
 
+	socket.on('disconnect', function(){
 
+	});
+});
 
+/*
 app.set('port', (process.env.PORT || 5000));
-
 app.use(express.static(__dirname + '/public'));
+*/
 
-
+/*
 fs.readFile('index.html', (err, html) => {
 	app.get('/', function(req, res){
 		res.setHeader('Content-type', 'text/html');
 		res.write(html);
-		
 		res.end();
 	});
 });
-
-//io.sockets.on('connection', function(socket) {
-//	connections.push(socket);
-//
-//	socket.on('disconnect', function(data){
-//		connections.splice(connections.indexOf(socket), 1);
-//	});
-//
-//	socket.on('send message', function(data){
-//		io.sockets.emit('new message', {msg: data});
-//	});
-//	
-//});
-
-app.listen(app.get('port'), function(){
-	console.log('Server running on port ', app.get('port'));
-})
+*/
