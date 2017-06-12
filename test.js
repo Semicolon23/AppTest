93b84665c6aaa019cc6f6fea258b30fee6a4c090
unknown <colinmcleodmunreo@mymail.mines.edu>
2017-06-12 11:27:41 -0600

    comments

--- test.js
+++ test.js
@@ -1,13 +1,16 @@
+//Set up server using the express framework. Listening on port 5000
 const express = require('express');
 const app = express();
 const http = require('http').Server(app).listen(5000);
-//const fs = require('fs');
 const io = require('socket.io')(http);
 
+//What happens when user loads main page. Can do other pages by doing
+// '/newPage' where newPage is the name of the page
 app.get('/', function(req,res){
 	res.sendFile(__dirname + '/index.html');
 });
 
+//Client Server Communication
 io.on('connection', function(socket){
 
 
@@ -17,10 +20,27 @@ io.on('connection', function(socket){
 	});
 });
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 */
+
+//const fs = require('fs');
 
 /*
 fs.readFile('index.html', (err, html) => {
