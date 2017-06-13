d34632d2aea778be829708a2a897d9476f33e0e5
unknown <colinmcleodmunreo@mymail.mines.edu>
2017-06-13 12:18:21 -0600

    Added comments to some sections

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var app = express();
 var http = require('http').Server(app).listen(5000);
 var io = require('socket.io')(http);
 
+//Array of connected users
 connections=[];
+
 //What happens when user loads main page. Can do other pages by doing
 // '/newPage' where newPage is the name of the page
 app.get('/', function(req,res){
@@ -13,6 +15,7 @@ app.get('/', function(req,res){
 
 //Client Server Communication
 io.on('connection', function(socket){
+	//Prevents more than two connections
 	if(connections.length < 2) {
 		connections.push(socket);
 		console.log('** %s players connected **', connections.length);
@@ -21,60 +24,29 @@ io.on('connection', function(socket){
 		console.log('** They were forcefully removed from the server **');
 		socket.disconnect();
 	};
-	
+	//Basically Pipelines, takes data from client 1 and pushes
+	//it through to client 2.	
+
+
 	socket.on('cardStat', function(dmg, health){
 
 	});
 
-	socket.on('cardLoc', function(oldLoc, newLoc){
-
-	});
-
+	//Pipe for board state. Takes client 1's board and puts it
+	//in client 2's enemy board location & vice versa.
 	socket.on('boardUpdate', function(data){
 		socket.broadcast.emit('boardUpdate', data);
-		console.log('sent data' + data);
 	});
 
+	//Pipe for phase indicator state. When phase indicator is
+	//updated on either client it is updated on other client too.
 	socket.on('phaseIndicator', function(data){
 		socket.broadcast.emit('phaseIndicator', data);
 	});
 
-
-	
+	//Part of player cap / dev testing.
 	socket.on('disconnect', function(){
 		connections.splice(connections.indexOf(socket), 1);
 		console.log('** %s players connected **', connections.length);
 	});
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-app.set('port', (process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
-*/
-
-//const fs = require('fs');
-
-/*
-fs.readFile('index.html', (err, html) => {
-	app.get('/', function(req, res){
-		res.setHeader('Content-type', 'text/html');
-		res.write(html);
-		res.end();
-	});
-});
-*/
