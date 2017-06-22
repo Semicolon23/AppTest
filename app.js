849e09fc82cf6a5ba6aabb03b434f96bc29a5b98
corbinbaker <corbinbaker890@gmail.com>
2017-06-21 20:39:28 -0600

    deckInit was never implemented, began coding deckInit

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ io.on('connection', function(socket){
 	socket.on('deckInit', function(data){
 	//DB REF GOES HERE, PUT CARDS INTO VAR DECK
 		
-pg.defaults.ssl = true;
+	pg.defaults.ssl = true;
 	pg.connect(process.env.postgres://zzfoijmjnpckbt:72b109361166e43307b0c3e29c50e4f44578a77d15398669a70e578b11f80ebf@ec2-23-23-93-255.compute-1.amazonaws.com:5432/d5j5918lt7is6t'),
 	function(err, client) 
 	{
